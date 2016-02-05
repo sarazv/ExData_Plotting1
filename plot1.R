d6570138fa94810e1a93efcdfd0bcb16ca683ecb
sarazv <sarazv@gmail.com>
2016-02-05 13:50:34 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,8 +1,8 @@
 hpc <- read.csv("./household_power_consumption.txt", sep = ";", header = TRUE) # read file
 
-hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
-df <- hpc[(hpc$Date=="2007-02-01") | (hpc$Date=="2007-02-02"),]   # subset from data
-df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y") 
+df <- hpc[(hpc$Date=="2007-02-01") | (hpc$Date=="2007-02-02"),]   # subset the desired dates from the data
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power)) # at first Global_active_power is defined as a factor, we need to define it as a numeric
 png(filename = "plot1.png")
 hist(df$Global_active_power, col = "red", main= "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
